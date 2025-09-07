47e4be7f0a3eac0a3780d7fc7c6fe49c8c60728f
Yarronius <yarronius@yandex.ru>
2025-09-07 14:05:20 +0300

    Update test_negate.c

--- src/tests/test_negate.c
+++ src/tests/test_negate.c
@@ -1,69 +1,69 @@
 #include "test.h"
 
-START_TEST(s21_negate_test_1) {
+START_TEST(negate_test_1) {
   float num = 3.14777;
   float res;
 
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_negate(value, &result);
-  s21_from_decimal_to_float(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = negate(value, &result);
+  from_decimal_to_float(result, &res);
 
   ck_assert_float_eq(res, -3.14777);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_negate_test_2) {
+START_TEST(negate_test_2) {
   float num = -3.14777;
   float res;
 
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_negate(value, &result);
-  s21_from_decimal_to_float(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = negate(value, &result);
+  from_decimal_to_float(result, &res);
 
   ck_assert_float_eq(res, 3.14777);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_negate_test_3) {
+START_TEST(negate_test_3) {
   float num = 36532.147;
   float res;
 
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_negate(value, &result);
-  s21_from_decimal_to_float(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = negate(value, &result);
+  from_decimal_to_float(result, &res);
 
   ck_assert_float_eq(res, -36532.147);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_negate_test_4) {
+START_TEST(negate_test_4) {
   float num = -36532.147;
   float res;
 
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_negate(value, &result);
-  s21_from_decimal_to_float(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = negate(value, &result);
+  from_decimal_to_float(result, &res);
 
   ck_assert_float_eq(res, 36532.147);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_negate_test_5) {
-  s21_decimal value;
-  s21_decimal result;
-  s21_decimal expected;
+START_TEST(negate_test_5) {
+  decimal value;
+  decimal result;
+  decimal expected;
   value.bits[3] = 0b00000000000000000000000000000000;
   value.bits[2] = 0b11111111111111111111111111111111;
   value.bits[1] = 0b11111111111111111111111111111111;
@@ -73,7 +73,7 @@ START_TEST(s21_negate_test_5) {
   expected.bits[2] = 0b11111111111111111111111111111111;
   expected.bits[1] = 0b11111111111111111111111111111111;
   expected.bits[0] = 0b11111111111111111111111111111111;
-  int code = s21_negate(value, &result);
+  int code = negate(value, &result);
 
   ck_assert_int_eq(result.bits[3], expected.bits[3]);
   ck_assert_int_eq(result.bits[2], expected.bits[2]);
@@ -83,10 +83,10 @@ START_TEST(s21_negate_test_5) {
 }
 END_TEST
 
-START_TEST(s21_negate_test_6) {
-  s21_decimal value;
-  s21_decimal result;
-  s21_decimal expected;
+START_TEST(negate_test_6) {
+  decimal value;
+  decimal result;
+  decimal expected;
   value.bits[3] = 0b10000000000000000000000000000000;
   value.bits[2] = 0b11111111110110001011101110110011;
   value.bits[1] = 0b11111101010101011100001010111111;
@@ -96,7 +96,7 @@ START_TEST(s21_negate_test_6) {
   expected.bits[2] = 0b11111111110110001011101110110011;
   expected.bits[1] = 0b11111101010101011100001010111111;
   expected.bits[0] = 0b11100010100010110101010101111011;
-  int code = s21_negate(value, &result);
+  int code = negate(value, &result);
 
   ck_assert_int_eq(result.bits[3], expected.bits[3]);
   ck_assert_int_eq(result.bits[2], expected.bits[2]);
@@ -106,10 +106,10 @@ START_TEST(s21_negate_test_6) {
 }
 END_TEST
 
-START_TEST(s21_negate_test_7) {
-  s21_decimal value;
-  s21_decimal result;
-  s21_decimal expected;
+START_TEST(negate_test_7) {
+  decimal value;
+  decimal result;
+  decimal expected;
   value.bits[3] = 0b00000000000000000000000000000000;
   value.bits[2] = 0b00000000000000000000000000000000;
   value.bits[1] = 0b00000000000000000000000000000000;
@@ -119,7 +119,7 @@ START_TEST(s21_negate_test_7) {
   expected.bits[2] = 0b00000000000000000000000000000000;
   expected.bits[1] = 0b00000000000000000000000000000000;
   expected.bits[0] = 0b00000000000000000000000000000000;
-  int code = s21_negate(value, &result);
+  int code = negate(value, &result);
 
   ck_assert_int_eq(result.bits[3], expected.bits[3]);
   ck_assert_int_eq(result.bits[2], expected.bits[2]);
@@ -129,12 +129,12 @@ START_TEST(s21_negate_test_7) {
 }
 END_TEST
 
-START_TEST(s21_negate_test_8) {
+START_TEST(negate_test_8) {
   float num = -36532.147;
 
-  s21_decimal value;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_negate(value, NULL);
+  decimal value;
+  from_float_to_decimal(num, &value);
+  int code = negate(value, NULL);
 
   ck_assert_int_eq(code, 1);
 }
@@ -145,13 +145,14 @@ Suite *test_negate(void) {
   TCase *tc = tcase_create("test_negate");
   suite_add_tcase(suite, tc);
 
-  tcase_add_test(tc, s21_negate_test_1);
-  tcase_add_test(tc, s21_negate_test_2);
-  tcase_add_test(tc, s21_negate_test_3);
-  tcase_add_test(tc, s21_negate_test_4);
-  tcase_add_test(tc, s21_negate_test_5);
-  tcase_add_test(tc, s21_negate_test_6);
-  tcase_add_test(tc, s21_negate_test_7);
-  tcase_add_test(tc, s21_negate_test_8);
+  tcase_add_test(tc, negate_test_1);
+  tcase_add_test(tc, negate_test_2);
+  tcase_add_test(tc, negate_test_3);
+  tcase_add_test(tc, negate_test_4);
+  tcase_add_test(tc, negate_test_5);
+  tcase_add_test(tc, negate_test_6);
+  tcase_add_test(tc, negate_test_7);
+  tcase_add_test(tc, negate_test_8);
   return suite;
+
 }
