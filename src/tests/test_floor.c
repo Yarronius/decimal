47a9a213243be71774052e99f08873d03c8f2f31
Yarronius <yarronius@yandex.ru>
2025-09-07 14:01:13 +0300

    Update test_floor.c

--- src/tests/test_floor.c
+++ src/tests/test_floor.c
@@ -1,131 +1,131 @@
 #include "test.h"
 
-START_TEST(s21_floor_test_1) {
+START_TEST(floor_test_1) {
   float num = 3.14;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, 3);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_2) {
+START_TEST(floor_test_2) {
   float num = -3.14;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, -4);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_3) {
+START_TEST(floor_test_3) {
   float num = 1234.945;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, 1234);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_4) {
+START_TEST(floor_test_4) {
   float num = -1234.94;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, -1235);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_5) {
+START_TEST(floor_test_5) {
   float num = 1234;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, 1234);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_6) {
+START_TEST(floor_test_6) {
   float num = -1234;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, -1234);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_7) {
+START_TEST(floor_test_7) {
   float num = 0;
   int res;
-  s21_decimal value;
-  s21_decimal result;
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, &result);
-  s21_from_decimal_to_int(result, &res);
+  decimal value;
+  decimal result;
+  from_float_to_decimal(num, &value);
+  int code = floor(value, &result);
+  from_decimal_to_int(result, &res);
 
   ck_assert_int_eq(res, 0);
   ck_assert_int_eq(code, 0);
 }
 END_TEST
 
-START_TEST(s21_floor_test_8) {
+START_TEST(floor_test_8) {
   float num = 45896.21;
-  s21_decimal value;
+  decimal value;
 
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, NULL);
+  from_float_to_decimal(num, &value);
+  int code = floor(value, NULL);
 
   ck_assert_int_eq(code, 1);
 }
 END_TEST
 
-START_TEST(s21_floor_test_9) {
+START_TEST(floor_test_9) {
   float num = -45896.21;
-  s21_decimal value;
+  decimal value;
 
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, NULL);
+  from_float_to_decimal(num, &value);
+  int code = floor(value, NULL);
 
   ck_assert_int_eq(code, 1);
 }
 END_TEST
 
-START_TEST(s21_floor_test_10) {
+START_TEST(floor_test_10) {
   float num = 0;
-  s21_decimal value;
+  decimal value;
 
-  s21_from_float_to_decimal(num, &value);
-  int code = s21_floor(value, NULL);
+  from_float_to_decimal(num, &value);
+  int code = floor(value, NULL);
 
   ck_assert_int_eq(code, 1);
 }
@@ -136,16 +136,17 @@ Suite *test_floor() {
   TCase *tc = tcase_create("test_floor");
   suite_add_tcase(suite, tc);
 
-  tcase_add_test(tc, s21_floor_test_1);
-  tcase_add_test(tc, s21_floor_test_2);
-  tcase_add_test(tc, s21_floor_test_3);
-  tcase_add_test(tc, s21_floor_test_4);
-  tcase_add_test(tc, s21_floor_test_5);
-  tcase_add_test(tc, s21_floor_test_6);
-  tcase_add_test(tc, s21_floor_test_7);
-  tcase_add_test(tc, s21_floor_test_8);
-  tcase_add_test(tc, s21_floor_test_9);
-  tcase_add_test(tc, s21_floor_test_10);
+  tcase_add_test(tc, floor_test_1);
+  tcase_add_test(tc, floor_test_2);
+  tcase_add_test(tc, floor_test_3);
+  tcase_add_test(tc, floor_test_4);
+  tcase_add_test(tc, floor_test_5);
+  tcase_add_test(tc, floor_test_6);
+  tcase_add_test(tc, floor_test_7);
+  tcase_add_test(tc, floor_test_8);
+  tcase_add_test(tc, floor_test_9);
+  tcase_add_test(tc, floor_test_10);
 
   return suite;
+
 }
